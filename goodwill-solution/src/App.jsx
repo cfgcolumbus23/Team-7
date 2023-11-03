74157e8a9c5de4388c2d919335c7b3dee8164011
Nick Topacio <topacio.3@osu.edu>
2023-11-03 16:04:59 -0400

    Init repo with React boilerplate

---
+++ goodwill-solution/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/chat" element={<h1>Chat</h1>} />
+        <Route path="/announcements" element={<h1>Announcements</h1>} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
