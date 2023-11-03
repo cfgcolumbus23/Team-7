90882fc863217b802a5d4a5c9b13547e0eba2f64
VamseeC21 <112673425+VamseeC21@users.noreply.github.com>
2023-11-03 17:29:02 -0400

    Added Home and Nav

--- goodwill-solution/src/App.jsx
+++ goodwill-solution/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/chat" element={<h1>Chat</h1>} />
         <Route path="/announcements" element={<h1>Announcements</h1>} />
       </Routes>
