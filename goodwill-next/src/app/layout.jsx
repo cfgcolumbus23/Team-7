f3a857fbb4932463d94fb07d747f8c28223dcbc6
VamseeC21 <112673425+VamseeC21@users.noreply.github.com>
2023-11-03 23:16:30 -0400

    Added home page stuff

--- goodwill-next/src/app/layout.jsx
+++ goodwill-next/src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './navbar'
+import Tile from './tiles'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <Tile />
+      {children}
+    </div>
   )
 }
