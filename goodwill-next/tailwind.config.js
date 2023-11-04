8b93889d5b0c0e27bbf9776028550795c27e7c8d
VamseeC21 <112673425+VamseeC21@users.noreply.github.com>
2023-11-03 20:27:48 -0400

    Add changes

--- goodwill-next/tailwind.config.js
+++ goodwill-next/tailwind.config.js
@@ -12,7 +12,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      "goodwill-blue": "#1946AA",
+      colors: {
+        "goodwill-blue": "#1946AA",
+      },
     },
   },
   plugins: [],
