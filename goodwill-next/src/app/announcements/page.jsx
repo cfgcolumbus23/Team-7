953b87547bf0ca9b1127718ec4d0238733b3277e
“solomon <sologreenbruh@gmail.com>
2023-11-04 08:19:45 -0400

    final updates for announcements

--- goodwill-next/src/app/announcements/page.jsx
+++ goodwill-next/src/app/announcements/page.jsx
@@ -7,7 +7,7 @@ export default function Page() {
   const newsRef = useRef(null);
   const careerRef = useRef(null);
   const courseRef = useRef(null);
-
+  
   function handleNewsClick() {
     newsRef.current?.scrollIntoView({ behavior: "smooth" });
   }
@@ -21,22 +21,22 @@ export default function Page() {
   return (
     <div className="">
       <div className="flex justify-around mt-48 mb-96 p-1 mx-4">
-      <button className="text-black rounded-lg border border-2 p-2 bg-goodwill-blue text-white "onClick={handleNewsClick}>Go to News
+      <button className="text-xl text-black rounded-lg border-2 p-2 bg-goodwill-blue text-white w-48 h-16 "onClick={handleNewsClick}>News
         </button>
-        <button className="text-black rounded-lg border border-2 p-2 bg-goodwill-blue text-white " onClick={handleCareerClick}>Go to Careers
+        <button className="text-xl text-black rounded-lg border-2 p-2 bg-goodwill-blue text-white w-48 h-16" onClick={handleCareerClick}>Careers
         </button>
-        <button className="text-black rounded-lg border border-2 p-2 bg-goodwill-blue text-white " onClick={handleOppoClick}>Go to Opportunities
+        <button className="text-xl text-black rounded-lg border-2 p-2 bg-goodwill-blue text-white w-48 h-16 " onClick={handleOppoClick}>Opportunities
         </button>
       </div>
         <div ref={newsRef} className="grid grid-cols-3 mt-96 pt-52 mb-96 gap-4 text-center items-center justify-center">
         <div><NewsCard 
-            info = "Click here to read more about the carrer at Goodwill!"
+            info = "Click here to read more about the carrer at Goodwill."
             title = "Company Info"
             ref = {careerRef} 
           />
           </div>
           <div><NewsCard 
-            info = "Click here to read about career modules at Goodwill!"
+            info = "Click here to read about career modules at Goodwill."
             title = "Career Modules"
           />
           </div>
@@ -46,11 +46,29 @@ export default function Page() {
           />
           </div>
         </div>
-        <div ref={careerRef} className="grid grid-cols-3 mt-96 pt-52 mb-96 gap-4 text-center">
+        <div ref={careerRef} className="grid grid-cols-3 mt-96 pt-52  mb-96 gap-4 text-center">
         <div><NewsCard 
-            info = "Click here to read more about the latest education opportunities at Goodwill!"
+            info = "Click here to read more about the latest career opportunities at Goodwill."
             title = "Education News"
             ref = {careerRef} 
+          />
+          </div>
+          <div><NewsCard 
+            info = "Click here to learn more about the carrer modules at Goodwill."
+            title = "Education Modules"
+          />
+          </div>
+          <div><NewsCard 
+            info = "Click here to read more about the work at Goodwill!"
+            title = "Education"
+          />
+          </div>
+        </div>
+        <div ref={courseRef} className="grid grid-cols-3 mt-96 pt-52 mb-96 gap-4 text-center"> 
+          <div><NewsCard 
+            info = "Click here to read more about the latest education opportunities at Goodwill."
+            title = "Education News"
+            ref = {courseRef}
           />
           </div>
           <div><NewsCard 
@@ -59,25 +77,7 @@ export default function Page() {
           />
           </div>
           <div><NewsCard 
-            info = "Click here to read more about the latest education opportunities at Goodwill!"
-            title = "Education"
-          />
-          </div>
-        </div>
-        <div ref={courseRef} className="grid grid-cols-3 mt-96 pt-52 mb-96 gap-4 text-center"> 
-          <div><NewsCard 
-            info = "Click here to read more about the latest education opportunities at Goodwill!"
-            title = "Education"
-            ref = {courseRef}
-          />
-          </div>
-          <div><NewsCard 
-            info = "Click here to read more about the latest education opportunities at Goodwill!"
-            title = "Education"
-          />
-          </div>
-          <div><NewsCard 
-            info = "Click here to read more about the latest education opportunities at Goodwill!"
+            info = "Click here to read more about the latest education opportunities at Goodwill."
             title = "Education"
           />
         </div>
