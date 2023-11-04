48613ed1a3a52ded9b8908eb52116cf8ddb6ba19
VamseeC21 <112673425+VamseeC21@users.noreply.github.com>
2023-11-04 04:25:47 -0400

    Changed tile home page since windows not compatible

--- goodwill-next/src/app/components/Tiles.jsx
+++ goodwill-next/src/app/components/Tiles.jsx
@@ -4,7 +4,7 @@ const Tile = () => {
         <div className="flex items-center justify-center">
         <div className="bg-gray-200 shadow-lg rounded-lg p-3 m-4 hover:bg-gray-300 hover:cursor-pointer">
             <div className="w-72 h-72 bg-cover bg-center">
-            <img src="/announcements.jpeg" alt="picture" className="h-2/3 w-full rounded-md"/>
+            <img src="/announcements_image.jpeg" alt="picture" className="h-2/3 w-full rounded-md"/>
             <p className="text-black mt-1">Click here for the announcements page.</p>
             </div>
         </div>
