dae5820138b952801515909de6909a14a0be41b9
“solomon <sologreenbruh@gmail.com>
2023-11-04 02:40:40 -0400

    removing issues with documentation

--- goodwill-next/src/app/announcements/page.jsx
+++ goodwill-next/src/app/announcements/page.jsx
@@ -1,13 +1,9 @@
 import NewsCard from "./../components/NewsCard";
 
-/**
- * Renders a page with a grid of NewsCard components.
- * @returns {JSX.Element} The Page component.
- */
 export default function Page() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="grid grid-cols-3 gap-20 gap-y-10 w-2/3">
+      <div className="margin-top: 20px grid grid-cols-3 gap-20 gap-y-10 w-2/3">
         <NewsCard
         info="Click here for more information about the "
         title="TEST"
