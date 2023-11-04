d9e591e3ab4dfd91fc3a51eef325eade2eaee545
Nick Topacio <topacio.3@osu.edu>
2023-11-04 00:27:09 -0400

    Add footer web responsiveness

--- goodwill-next/src/app/components/Footer.jsx
+++ goodwill-next/src/app/components/Footer.jsx
@@ -1,3 +1,18 @@
-import React from "react";
+"use client";
+import { React, useState, useEffect } from "react";
+import FooterDesktop from "./FooterDesktop";
+import FooterMobile from "./FooterMobile";
 
-export default function Footer() {}
+export default function Footer() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  function handleResize() {
+    window.innerWidth < 768 ? setIsMobile(true) : setIsMobile(false);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
+
+  return <div>{isMobile ? <FooterMobile /> : <FooterDesktop />}</div>;
+}
