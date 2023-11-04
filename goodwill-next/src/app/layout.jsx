7096f2fc1819c50a3b4706896fe49eb3bfa4905c
Nick Topacio <topacio.3@osu.edu>
2023-11-04 00:45:15 -0400

    Remove footer and navbar

--- goodwill-next/src/app/layout.jsx
+++ goodwill-next/src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,9 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <body>
-        <Navbar />
         <main>{children}</main>
-        <Footer />
       </body>
     </html>
   );
