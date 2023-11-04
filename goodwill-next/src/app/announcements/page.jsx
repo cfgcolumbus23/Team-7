0dc3132b36fe37251a95baadca948df88583d29b
“solomon <sologreenbruh@gmail.com>
2023-11-04 02:54:00 -0400

    Updating documentation with issues

--- goodwill-next/src/app/announcements/page.jsx
+++ goodwill-next/src/app/announcements/page.jsx
@@ -3,7 +3,7 @@ import NewsCard from "./../components/NewsCard";
 export default function Page() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="margin-top: 20px grid grid-cols-3 gap-20 gap-y-10 w-2/3">
+      <div className=" grid grid-cols-3 gap-20 gap-y-10 w-2/3">
         <NewsCard
         info="Click here for more information about the "
         title="TEST"
