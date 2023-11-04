c7d52ca34050de13e1e7638e22c36d5e0b46d760
Nick Topacio <topacio.3@osu.edu>
2023-11-04 05:31:12 -0400

    Add home card animations

--- goodwill-next/src/app/announcements/page.jsx
+++ goodwill-next/src/app/announcements/page.jsx
@@ -5,41 +5,41 @@ export default function Page() {
     <div className="w-full h-screen flex items-center justify-center">
       <div className=" grid grid-cols-3 gap-20 gap-y-10 w-2/3">
         <NewsCard
-        info="Click here for more information about the "
-        title="TEST"
+          info="Click here for more information about the "
+          title="TEST"
         />
         <NewsCard
-        info="Click here for more information about the "
-        title="TEST"
+          info="Click here for more information about the "
+          title="TEST"
         />
         <NewsCard
-        info="Click here for more information about the "
-        title="TEST"
+          info="Click here for more information about the "
+          title="TEST"
         />
         <NewsCard
-        info="Click here for more information about the "
-        title="TEST"
-         />
-         <NewsCard
-        info="Click here for more information about the "
-        title="TEST"
-         />
-         <NewsCard
-        info="Click here for more information about the "
-        title="TEST"
-         />
-         <NewsCard
-        info="Click here for more information about the "
-        title="TEST"
-         />
-         <NewsCard
-        info="Click here for more information about the "
-        title="TEST"
-         />
-         <NewsCard
-        info="Click here for more information about the "
-        title="TEST"
-         />
+          info="Click here for more information about the "
+          title="TEST"
+        />
+        <NewsCard
+          info="Click here for more information about the "
+          title="TEST"
+        />
+        <NewsCard
+          info="Click here for more information about the "
+          title="TEST"
+        />
+        <NewsCard
+          info="Click here for more information about the "
+          title="TEST"
+        />
+        <NewsCard
+          info="Click here for more information about the "
+          title="TEST"
+        />
+        <NewsCard
+          info="Click here for more information about the "
+          title="TEST"
+        />
       </div>
     </div>
   );
--- goodwill-next/src/app/components/Tiles.jsx
+++ goodwill-next/src/app/components/Tiles.jsx
@@ -11,15 +11,15 @@ const Tile = () => {
           router.push("/announcements");
         }}
       >
-        <div className="bg-gray-200 shadow-lg rounded-lg p-3 m-4 hover:bg-gray-300 hover:cursor-pointer">
+        <div className="bg-gray-200 shadow-lg transform transition duration-300 hover:scale-110 rounded-lg p-3 m-4 hover:bg-gray-300 hover:cursor-pointer">
           <div className="w-80 md:w-48 lg:w-72 h-72 bg-cover bg-center">
             <img
               src="/announcements_image.jpeg"
               alt="picture"
               className="h-2/3 w-full rounded-md"
             />
-            <p className="text-black mt-1">
-              Click here for the announcements page.
+            <p className="text-black text-4xl md:text-xl lg:text-3xl mt-8">
+              ANNOUNCEMENTS
             </p>
           </div>
         </div>
@@ -30,28 +30,30 @@ const Tile = () => {
           router.push("/chat");
         }}
       >
-        <div className="bg-gray-200 shadow-lg rounded-lg p-3 m-4 hover:bg-gray-300 hover:cursor-pointer">
+        <div className="bg-gray-200 transform transition duration-300 hover:scale-110 shadow-lg rounded-lg p-3 m-4 hover:bg-gray-300 hover:cursor-pointer">
           <div className="w-80 md:w-48 lg:w-72 h-72 bg-contain bg-center">
             <img
               src="/chat.webp"
               alt="picture"
               className="h-2/3 w-full rounded-md"
             />
-            <p className="text-black mt-1">
-              Click here to chat with a professional.
+            <p className="text-black text-4xl md:text-2xl lg:text-3xl mt-3">
+              CHAT WITH A PROFESSIONAL
             </p>
           </div>
         </div>
       </button>
       <a href="https://www.goodwillcolumbus.org/services/for-individuals/find-a-job/">
-        <div className="bg-gray-200 shadow-lg rounded-lg p-3 m-4 hover:bg-gray-300 hover:cursor-pointer">
+        <div className="bg-gray-200 shadow-lg transform transition duration-300 hover:scale-110 rounded-lg p-3 m-4 hover:bg-gray-300 hover:cursor-pointer">
           <div className="w-80 md:w-48 lg:w-72 h-72 bg-cover bg-center">
             <img
               src="/job-opportunity.jpeg"
               alt="picture"
               className="h-2/3 w-full rounded-md"
             />
-            <p className="text-black mt-1">Click here for job oppurtunities.</p>
+            <p className="text-black text-4xl md:text-2xl lg:text-3xl mt-3 text-center">
+              JOB OPPORTUNITIES
+            </p>
           </div>
         </div>
       </a>
