5c9f8f33fc3f6073006d20b027723a1fcb2ac77a
miten <57693631+m1ten@users.noreply.github.com>
2023-11-03 23:25:28 -0400

    Update layout.jsx

--- goodwill-next/src/app/layout.jsx
+++ goodwill-next/src/app/layout.jsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <body>
-        {/* <Navbar /> */}
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
